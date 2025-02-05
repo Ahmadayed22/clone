72000ef5b090f06480031b0bbeec144b08f21e67
Ahmadayed22 <ahmad.ayed461@gmail.com>
2025-02-05 17:29:15 +0300

    chane backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://clone-phi-green.vercel.app/',
         secure: false,
       },
     },
